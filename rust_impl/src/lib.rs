b71eaab74e6f12bd1c45fd38ff17ab5ebce3e5ce
persona-mp3 <randomnobscurebs@gmail.com>
2026-04-09 23:18:34 +0100

    feat: parse history file using sqlite

--- rust_impl/src/lib.rs
+++ rust_impl/src/lib.rs
@@ -1,5 +1,8 @@
-use std::fs;
+#![allow(dead_code)]
 use std::path::PathBuf;
+use std::process::Command;
+
+use rusqlite::{Connection, Result};
 
 use home;
 #[derive(Debug)]
@@ -35,6 +38,15 @@ impl Config {
 pub const CHROME_HISTORY_FILE: &'static str =
     "Library/Application Support/Google/Chrome/Default/History";
 
+#[derive(Debug)]
+struct VisitedUrl {
+    id: i32,
+    link_url_id: i32,
+    top_level_url: String,
+    frame_url: String,
+    visit_count: i32,
+}
+
 pub fn get_browsing_history(config: &Config) -> Result<&str, Box<dyn std::error::Error>> {
     let file_path = match config.browser.as_str() {
         "chrome" => CHROME_HISTORY_FILE,
@@ -45,15 +57,26 @@ pub fn get_browsing_history(config: &Config) -> Result<&str, Box<dyn std::error:
 
     let full_path = homedir.join(file_path);
 
-    let metadata = match fs::metadata(&full_path) {
-        Ok(md) => md,
-        Err(err) => {
-            eprintln!("Could not get metdata for file: {:?}", full_path);
-            return Err(err.into());
-        }
-    };
+    let temp_file = PathBuf::from("temp_history.db");
+    Command::new("cp").args([&full_path, &temp_file]).output()?;
 
-    let fsize = metadata.len();
-    println!("fsize: {}", fsize);
+    // Docs: https://crates.io/crates/rusqlite/
+    let conn = Connection::open(&temp_file)?;
+    let mut stmt = conn.prepare("SELECT * FROM visited_links ORDER BY id DESC LIMIT (?1)")?;
+    let visited_urls_iter = stmt.query_map([5i32], |row| {
+        Ok(VisitedUrl {
+            id: row.get(0)?,
+            link_url_id: row.get(1)?,
+            top_level_url: row.get(2)?,
+            frame_url: row.get(3)?,
+            visit_count: row.get(4)?,
+        })
+    })?;
+
+    for url in visited_urls_iter {
+        println!("{:?}\n", url.unwrap());
+    }
+
+    Command::new("rm").arg(&temp_file).output()?;
     Ok("")
 }
