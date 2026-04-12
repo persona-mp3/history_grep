d0d3dad8e9be7948108af1c4c9ca7c74a2eca0ca
persona-mp3 <randomnobscurebs@gmail.com>
2026-04-12 05:24:19 +0100

    feat: fzf parsing

--- rust_impl/src/lib.rs
+++ rust_impl/src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 use std::env;
+use std::io::{Read, Write};
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use rusqlite::{Connection, Result};
 
@@ -10,6 +11,7 @@ use home;
 pub struct Config {
     pub browser: String,
     pub limit: u32,
+    pub fzf: String,
 }
 
 impl Config {
@@ -17,13 +19,14 @@ impl Config {
         Config {
             browser: String::from("chrome"),
             limit: 600,
+            fzf: String::from("fzf"),
         }
     }
 
     pub fn build(args: &[String]) -> Result<Config, Box<dyn std::error::Error>> {
-        if args.len() < 3 {
+        if args.len() < 4 {
             return Err("Not enough arguments\
-                hist <browser-name> <limit>
+                hist <browser-name> <limit> <fzf>
                 "
             .into());
         }
@@ -33,6 +36,7 @@ impl Config {
         Ok(Config {
             browser: browser.clone(),
             limit: limit,
+            fzf: String::from("fzf"),
         })
     }
 }
@@ -43,7 +47,7 @@ pub const CHROME_HISTORY_FILE_MACOS: &'static str =
 pub const CHROME_HISTORY_FILE_LINUX: &'static str = ".config/google-chrome/Default/History";
 
 #[derive(Debug)]
-struct VisitedUrl {
+pub struct VisitedUrl {
     id: i32,
     link_url_id: i32,
     top_level_url: String,
@@ -71,7 +75,10 @@ pub fn get_os() -> OSArch {
     os_arch
 }
 
-pub fn parse_browing_history(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+/// Matches the OS the application is running on an automatically resolves the possible file-path
+/// of the browser. If successful, it returns the temporary  file path, as `.temp_chrome_history.db`
+/// Callers need to call `cleanup()` to delete the file
+pub fn copy_browing_history(config: &Config) -> Result<PathBuf, Box<dyn std::error::Error>> {
     let os_arch = get_os();
 
     let browser_history_path: &str = match config.browser.as_str() {
@@ -94,7 +101,6 @@ pub fn parse_browing_history(config: &Config) -> Result<(), Box<dyn std::error::
     let temp_file = PathBuf::from(".temp_chrome_history.db");
     let browser_history_path = home_dir.join(&browser_history_path);
 
-    // NOTE: Do research if Rust translates it to Windows command
     let exit_status = Command::new("cp")
         .args([&browser_history_path, &temp_file])
         .output()?;
@@ -108,6 +114,13 @@ pub fn parse_browing_history(config: &Config) -> Result<(), Box<dyn std::error::
     println!("Copy was successfull");
     println!("exit_status -> {:?} ", exit_status);
 
+    Ok(temp_file)
+}
+
+/// Parses the browsing history from the temp_file path using sqlite, reading the rows into `VisitedUrl`
+pub fn parse_browsing_history(
+    temp_file: &PathBuf,
+) -> Result<Vec<VisitedUrl>, Box<dyn std::error::Error>> {
     let conn = match Connection::open(&temp_file) {
         Ok(conn) => conn,
         Err(err) => {
@@ -127,17 +140,88 @@ pub fn parse_browing_history(config: &Config) -> Result<(), Box<dyn std::error::
         })
     })?;
 
-    for url in visited_urls_iter {
-        println!("{:?}\n", url.unwrap());
+    let mut valid_data = vec![];
+    for v_url in visited_urls_iter {
+        valid_data.push(v_url.unwrap());
     }
 
-    // Cleanup
+    Ok(valid_data)
+}
+
+fn get_fzf(fzf: &String) -> Result<String, Box<dyn std::error::Error>> {
+    // TODO: Come back to this bit!
+    let exit_status = Command::new("which").arg(fzf).output()?;
+    if !exit_status.status.success() {
+        eprintln!("Could not get fzf_path for {}", fzf);
+        let stderr_msg = exit_status.stderr;
+        return Err(String::from_utf8_lossy(&stderr_msg).into());
+    }
+
+    let stdout = String::from_utf8(exit_status.stdout)?;
+    let stdout = stdout.trim().strip_suffix("\n");
+
+    match stdout {
+        Some(s) => Ok(s.to_string()),
+        None => {
+            eprintln!("Could not get executable path for fzf: {}", fzf);
+            Err("Could not get fzf path".into())
+        }
+    }
+}
+
+pub fn collect_input(
+    config: &Config,
+    temp_file: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let fzf_executable = get_fzf(&config.fzf)?;
+    let browsing_history = parse_browsing_history(temp_file)?;
+
+    // NOTE: I'd like it to panic here, so I can get a stack trace of what and where something
+    // failed
+    let mut fzf_process = Command::new(fzf_executable)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let mut fzf_stdin = if let Some(stdin) = fzf_process.stdin.take() {
+        stdin
+    } else {
+        return Err("Could not get stdin of fzf_process".into());
+    };
+
+    let mut fzf_stdout = if let Some(stdout) = fzf_process.stdout.take() {
+        stdout
+    } else {
+        return Err("Could not get stdout of fzf_process".into());
+    };
+
+    let mut all_urls: Vec<String> = vec![];
+    for url in browsing_history {
+        all_urls.push(url.frame_url);
+    }
+
+    let all_urls = all_urls.join("\n");
+
+    write!(fzf_stdin, "{}", all_urls).expect("Could not write to fzf's stdin!");
+    drop(fzf_stdin);
+    fzf_process.wait().expect("fzf exited with an error");
+
+    let mut buffer = String::new();
+    fzf_stdout
+        .read_to_string(&mut buffer)
+        .expect("Could not read from fzfs stdout!");
+
+    println!("User selected {}", buffer);
+    Ok(())
+}
+
+pub fn cleanup(temp_file: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
     let exit_status = Command::new("rm").arg(&temp_file).output()?;
     if !exit_status.status.success() {
         eprintln!("Failed to remove history file {:?}", temp_file);
         let stderr_msg = exit_status.stderr;
         return Err(String::from_utf8_lossy(&stderr_msg).into());
     }
-
     Ok(())
 }
--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -1,11 +1,11 @@
-// use rust_impl::{Config, get_browsing_history, parse_browing_history};
-use rust_impl::{Config, parse_browing_history};
+use rust_impl::{Config, copy_browing_history};
 use std::env;
 
 // NOTE: If you're on a linux distro, you need to install sqlite so the application
 // can interface with it. Spefically this was built on a late night cafee, on debian distro
 // and I ran `sudo apt-get install libsqlite3-dev`. Since I choose to use the `rusqlite` crate
 // it screams alot when it can't find it
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let config = if args.len() >= 3 {
@@ -14,7 +14,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Config::default()
     };
 
-    let _history = parse_browing_history(&config)?;
+    let temp_file = copy_browing_history(&config)?;
+    rust_impl::parse_browsing_history(&temp_file)?;
+    rust_impl::collect_input(&config, &temp_file)?;
+
+    rust_impl::cleanup(temp_file)?;
 
     // TODO:
     // [] Pipe content to fzf
